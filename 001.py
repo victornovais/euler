c78fbf44ad69cf07b3013b38f705b828ee3543e2
VictorNovais <victorh.novaisr@gmail.com>
2013-04-20 14:38:21 -0300

    PEP 8

--- 001.py
+++ 001.py
@@ -1,10 +1,12 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
+# multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+
 # Using the formula of the sum of first n numbers
-def sum(number, divisor):
+def sum_terms(number, divisor):
     number = int(number / divisor)
     return int(number * (number + 1) / 2 * divisor)
 
 limit = 999
-print(sum(limit,3) + sum(limit,5) - sum(limit,15))
+print(sum_terms(limit, 3) + sum_terms(limit, 5) - sum_terms(limit, 15))
--- 007.py
+++ 007.py
@@ -1,1 +1,19 @@
-# -*- coding:utf-8 -*-
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#What is the 10 001st prime number?
+from math import sqrt
+
+
+def is_prime(n):
+    for i in range(2, int(sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+        else:
+            return True
+
+primes = []
+n = 2
+while len(primes) <= 10001:
+    if is_prime(n):
+        primes.append(n)
+
+print primes[10000]
