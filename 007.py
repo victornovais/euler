c0a6a39c3d6df6a8a929ab30a685e95c799c1883
VictorNovais <victorh.novaisr@gmail.com>
2013-04-20 16:10:02 -0300

    Soluções 007 e 008

--- 007.py
+++ 007.py
@@ -1,19 +1,13 @@
-#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-#What is the 10 001st prime number?
-from math import sqrt
-
-
-def is_prime(n):
-    for i in range(2, int(sqrt(n) + 1)):
-        if n % i == 0:
-            return False
-        else:
-            return True
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+from tools import is_prime
 
 primes = []
-n = 2
-while len(primes) <= 10001:
+n = 3
+primes.append(2)
+while len(primes) <= 10000:
     if is_prime(n):
         primes.append(n)
+    n += 2
 
 print primes[10000]
--- tools.py
+++ tools.py
@@ -4,3 +4,15 @@ def is_palindromic(n):
         return True
     return False
 
+
+def is_prime(n):
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
+            return False
+    return True
