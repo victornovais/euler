601a0e27a48b5244382f0008c6401b0a00d9ddfd
VictorNovais <victorh.novaisr@gmail.com>
2013-04-23 20:56:20 -0300

    Soluções 013 e 016

--- 016.py
+++ 016.py
@@ -1,1 +1,6 @@
-# -*- coding:utf-8 -*-
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 21000?
+
+digits = map(int, str(pow(2, 1000)))
+
+print sum(digits)
